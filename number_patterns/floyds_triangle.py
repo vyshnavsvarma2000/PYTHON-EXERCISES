74b31fc1a59301e849b4ce65856c2f9d6d667091
Vyshnav S Varma <vyshnavsvarma2255@gmail.com>
2024-05-22 14:05:46 +0530

    floyd's triangle

---
+++ number_patterns/floyds_triangle.py
@@ -0,0 +1,12 @@
+def print_floyds_triangle(n):
+    num = 1
+    for i in range(1, n + 1):
+        for j in range(1, i + 1):
+            print(num, end=" ")
+            num += 1
+        print()
+
+
+# Example usage:
+print("Floyd's Triangle:")
+print_floyds_triangle(5)
